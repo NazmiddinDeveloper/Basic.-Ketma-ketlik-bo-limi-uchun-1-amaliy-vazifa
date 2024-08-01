e3d47eb027de81bfc22698071a8fbb6e163263b9
nazmiddin <nazmiddinsirojiddinov080@gmail.com>
2024-08-01 23:59:04 +0500

    3. Yoshni hisoblash

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿// 2. Valyuta konvertori
-System.Console.Write("Valyuta kiriting : ");
-string valyutaStr = Console.ReadLine();
-int valyuta = Convert.ToInt32(valyutaStr);
-int kurs = 12400;
-int qiymat = valyuta * kurs;
-System.Console.WriteLine($"Qiymat -> {qiymat} so'm"); 
+﻿// 3. Yoshni hisoblash
+
+System.Console.Write("Tug'ilgan yilingizni kiriting : ");
+string yearStr = Console.ReadLine();
+int year = Convert.ToInt32(yearStr);
+int currentYear = 2024;
+int age = currentYear - year;
+int days = age * 366;
+System.Console.WriteLine($"Output : {days}"); 
+
+
